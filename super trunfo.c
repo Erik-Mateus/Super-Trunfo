5cc170c593afa1b398968b7563fd0b4d471215a7
Erik-Mateus <rdrderikseixas@gmail.com>
2025-01-23 15:02:48 -0300

    adicionando entrada de dados

--- super trunfo.c
+++ super trunfo.c
@@ -19,12 +19,30 @@
 
 int main(){
 
-    
-    int idade;
+    char nome[20];
+    int numero,quantidade;
+    float forcaDaCarta;
 
-    printf("digite sua idade\n");
-    scanf(" %d", &idade);
+    printf("Digite seu Nome Em ate 20 caracteres\n");
+    scanf("%s", &nome);
 
-    printf("idade: %d\n", idade);
+    printf("Digite o seu numero e quantas cartas vc ira pegar\n");
+    scanf("%d", &numero);
+    scanf("%d", &quantidade);
+
+    printf("determine a forca da carta\n");
+    scanf("%f",&forcaDaCarta);
+
+
+
+    printf("      \n");
+    printf("Seu perfil: \n");
+    printf("      \n");
+
+    printf("Nome = %s  -  Jogador N* = %d \n",nome, numero);
+    printf("Quantidade de cartas = %d\n", quantidade);
+    printf("Forca da carta = %f\n",forcaDaCarta);
+
+    return 0;
 
 }
