d9f7edba0fb01c67f70d13ae2bb768220466164c
Erik-Mateus <rdrderikseixas@gmail.com>
2025-01-23 04:57:27 -0300

    changeNames

---
+++ super trunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// int main() {
+
+//     int idade = 18;
+//     float altura = 1.8;
+//     char sexo = 'M';
+//     char nome[20]= "Mateus";
+
+
+
+//     printf("%d\n", idade);
+//     printf("%f\n", altura);
+//     printf("%c\n", sexo);
+//     printf("%s\n", nome);
+
+//     return 0;
+// }
+
+int main(){
+
+    
+    int idade;
+
+    printf("digite sua idade\n");
+    scanf(" %d", &idade);
+
+    printf("idade: %d\n", idade);
+
+}
