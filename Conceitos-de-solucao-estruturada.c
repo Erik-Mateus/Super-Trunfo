8f693f069cfc83d07ca9c9b23e20d6dc073b5306
Erik-Mateus <rdrderikseixas@gmail.com>
2025-01-25 13:11:48 -0300

    teste git

--- Conceitos-de-solucao-estruturada.c
+++ Conceitos-de-solucao-estruturada.c
@@ -6,6 +6,13 @@ void entradaDeDados() {
     int idade;
     char nome[30];
     
-    scanf("%d", idade);
+    scanf("%d", &idade);
+
+}
+
+//teste git hub
+void saidaDeDados(){
+
+printf("oi rapaziada\n");
 
 }
